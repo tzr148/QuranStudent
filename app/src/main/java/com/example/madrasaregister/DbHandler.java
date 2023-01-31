17e350ae197e5dbf28b0c04b3cb4eb63070b9471
tzr148 <111836321+tzr148@users.noreply.github.com>
2023-01-31 22:48:56 +0500

    first commit

---
+++ app/src/main/java/com/example/madrasaregister/DbHandler.java
@@ -0,0 +1,119 @@
+package com.example.madrasaregister;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DbHandler extends  SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "madrasa.db";
+    private static final String TABLE_NAME = "student";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_ROLLNO = "roll_no";
+    private static final String COLUMN_JOINING = "joining";
+
+    private static final String REC_TABLE_NAME = "record";
+    private static final String REC_COLUMN_ID = "id";
+    private static final String COLUMN_STD_ID = "std_id";
+    private static final String COLUMN_SABAK = "sabak";
+    private static final String COLUMN_SABKI = "sabki";
+    private static final String COLUMN_DATE = "date";
+    private static final String COLUMN_MANZIL = "manzil";
+
+    public DbHandler(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+                String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_ROLLNO + " TEXT,"
+                + COLUMN_JOINING + " TEXT"
+                + ")";
+        db.execSQL(sql);
+
+        String sql2 = "CREATE TABLE IF NOT EXISTS " + REC_TABLE_NAME + "("
+                + REC_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_STD_ID + " INTEGER,"
+                + COLUMN_DATE + " TEXT,"
+                + COLUMN_SABAK + " INTEGER,"
+                + COLUMN_SABKI + " INTEGER,"
+                + COLUMN_MANZIL + " INTEGER,"
+                +"FOREIGN KEY ("+COLUMN_STD_ID +")"
+                +"REFERENCES student(id)"
+                + ")";
+        db.execSQL(sql2);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(sql);
+        String sql2 = "DROP TABLE IF EXISTS " + REC_TABLE_NAME;
+        db.execSQL(sql2);
+        onCreate(db);
+    }
+
+    public void insertStudent(Student student) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, student.getName());
+        values.put(COLUMN_ROLLNO, student.getRollNo());
+        values.put(COLUMN_JOINING, student.getJoining());
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public List<Student> selectAllStudents() {
+        List<Student> students = new ArrayList<>();
+        String sql = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+        if (cursor.moveToFirst()) {
+            do {
+                students.add(new Student(cursor.getInt(0),
+                        cursor.getString(1),
+                        cursor.getString(2),cursor.getString(3)));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return students;
+    }
+
+    public void insertRecord(Record record) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_STD_ID, record.getStdId());
+        values.put(COLUMN_DATE, record.getDate());
+        values.put(COLUMN_SABAK, record.getSabak());
+        values.put(COLUMN_SABKI, record.getSabki());
+        values.put(COLUMN_MANZIL, record.getManzil());
+        db.insert(REC_TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public ArrayList<Record> selectAllRecords(Integer id) {
+        ArrayList<Record> records = new ArrayList<>();
+        String sql = "SELECT * FROM " + REC_TABLE_NAME +" where std_id="+id;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+        if (cursor.moveToFirst()) {
+            do {
+                records.add(new Record(cursor.getInt(0),cursor.getInt(1),
+                        cursor.getString(2), cursor.getInt(3),
+                        cursor.getInt(4),cursor.getInt(5)));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return records;
+    }
+}
---
+++ app/src/main/java/com/example/madrasaregister/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.madrasaregister;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_add_std, btn_show_reocrds, btn_add_record;
+    DbHandler dbHandler;
+    ImageView git_link;
+    EditText rec_date, rec_sabak, rec_sabki, rec_manzil;
+    Spinner stdSpinner;
+    ArrayAdapter<Student> spinnerAdapter;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_add_std=findViewById(R.id.btn_add_std);
+        btn_show_reocrds= findViewById(R.id.btn_see_records);
+        btn_add_record= findViewById(R.id.btn_add_record);
+        rec_date = findViewById(R.id.txt_date);
+        rec_sabak = findViewById(R.id.txt_sabak);
+        rec_sabki = findViewById(R.id.txt_sabki);
+        rec_manzil = findViewById(R.id.txt_manzil);
+        git_link = findViewById(R.id.git_link);
+        dbHandler=new DbHandler(this);
+        List<Student> students = dbHandler.selectAllStudents();
+        spinnerAdapter = new ArrayAdapter<Student>(this, android.R.layout.simple_spinner_item, students);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        stdSpinner = findViewById(R.id.std_spinner);
+        stdSpinner.setAdapter(spinnerAdapter);
+        btn_add_std.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+                final View dialogView = inflater.inflate(R.layout.add_std_layout, null);
+                builder.setView(dialogView);
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        EditText name = dialogView.findViewById(R.id.std_name);
+                        EditText rollno = dialogView.findViewById(R.id.std_roll);
+                        EditText joining = dialogView.findViewById(R.id.std_date);
+                        Student std=new Student(0,name.getText().toString(),rollno.getText().toString(),joining.getText().toString());
+                        dbHandler.insertStudent(std);
+                        List<Student> students = dbHandler.selectAllStudents();
+                        spinnerAdapter = new ArrayAdapter<Student>(MainActivity.this, android.R.layout.simple_spinner_item, students);
+                        spinnerAdapter.notifyDataSetChanged();
+                        stdSpinner.setAdapter(spinnerAdapter);
+                        Toast.makeText(MainActivity.this, "Student Added Successfully!!", Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+
+        btn_show_reocrds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                Integer id = Integer.parseInt(stdSpinner.getSelectedItem().toString().split(":")[0]);
+                ArrayList<Record> records = dbHandler.selectAllRecords(id);
+                RecordAdapter dataAdapter = new RecordAdapter(MainActivity.this, records);
+                builder.setAdapter(dataAdapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"You have selected " + records.get(which),Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        btn_add_record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String date = rec_date.getText().toString();
+                Integer sabak =  Integer.parseInt(rec_sabak.getText().toString());
+                Integer sabki =  Integer.parseInt(rec_sabki.getText().toString());
+                Integer manzil =  Integer.parseInt(rec_manzil.getText().toString());
+                Integer stdId =  Integer.parseInt(stdSpinner.getSelectedItem().toString().split(":")[0]);
+                if (date.isEmpty() || sabak<1 || sabki<1 || sabki>30 || manzil<1 || manzil>30)
+                {
+                    Toast.makeText(MainActivity.this, "Record Not Added (Invalid Data)!!", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Record record = new Record(0,stdId,date,sabak,sabki,manzil);
+                    dbHandler.insertRecord(record);
+                    Toast.makeText(MainActivity.this, "Record Added Successfully!!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        git_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("https://github.com/AqibAmin/MadrasaRegister");
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
